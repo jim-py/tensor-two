cec2ca3a96b4911a754a38e040c8a2a8dae46b84
jim-py <jimmybee@vk.com>
2022-11-29 20:55:30 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,2 @@
+nums = list(map(float, input("Введите два числа: ").split()))
+print(f'Сумма: {nums[0] + nums[1]}\nРазница: {nums[0] - nums[1]}\nУмножение: {nums[0] * nums[1]}\nДеление: {nums[0] / nums[1]}\nСтепень: {nums[0] ** nums[1]}\nДеление по модулю: {nums[0] % nums[1]}\nЦелочисленное деление: {nums[0] // nums[1]}')
---
+++ 4.py
@@ -0,0 +1,3 @@
+import math
+
+print(f'Площадь круга: {round(math.pi * float(input("Введите радиус круга: ")) ** 2, 2)}')
